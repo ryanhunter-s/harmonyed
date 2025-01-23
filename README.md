cc76ca510ec491a2999925dc617150868f3669ee
Ryan Hunter S <ryanjhunter033@gmail.com>
2025-01-23 15:33:13 -0600

    End Clerk

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 
 - [x] Make it deploy
 - [x] Scaffold basic ui with mock data
-- [ ] Actually set up a database (vercel postgres)
-- [ ] Attach database to UI
-- [ ] Add authentication (w/ clerk)
+- [x] Tidy up build process
+- [x] Actually set up a database (vercel postgres)
+- [x] Attach database to UI
+- [x] Add authentication (w/ clerk)
 - [ ] Add image upload
+- [ ] "taint" (server-only)
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel router)
 - [ ] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
+export async function Images() {
+	const images = await db.query.posts.findMany({
+		orderBy: (model, {desc}) => desc(model.id)
+	});
+	return (
+		<div className="flex flex-wrap gap-4">
+			{images.map((image, index) => (
+				<div key={image.id} className="w-48">
+					<img src={image.url} />
+					<p>{image.name}</p>
+				</div>
+			))}
+		</div>
+	);
+}
+
 export default async function HomePage() {
-	
 	return (
 		<main className="flex justify-center">
-			<Link href="/dashboard/">
-				<h1>go to dashboard</h1>
-			</Link>
+			<SignedOut>
+				<div className="h-full w-full text-2x1">please Sign In above</div>
+			</SignedOut>
+			<SignedIn>
+				<Images/>
+			</SignedIn>
 		</main>
 	);
 }
