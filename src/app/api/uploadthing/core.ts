2e3d62a4cf5c1432467261e7cd4b3b8b158d9ea9
Ryan Hunter S <ryanjhunter033@gmail.com>
2025-01-27 11:23:27 -0600

    update data core

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,29 +1,23 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
 import { posts } from "~/server/db/schema";
 
 const f = createUploadthing();
 
-const auth = (req: NextApiRequest, res: NextApiResponse) => ({ id: "fakeId" }); // Fake auth function
-
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
 	imageUploader: f({
-		image: {maxFileSize: "4MB", maxFileCount: 10},
-	})
-	.middleware(async ({ req, res }) => {
-			const user = auth(req, res);
-			// If you throw, the user will not be able to upload
-			// eslint-disable-next-line @typescript-eslint/only-throw-error
-			if (!user) throw new UploadThingError("Unauthorized");
+		image: {maxFileSize: "4MB", maxFileCount: 10}})
+		.middleware(async ({ req }) => {
+		const user = auth();
 
-			// Whatever is returned here is accessible in onUploadComplete as `metadata`
-			return { userId: user.id };
+		// eslint-disable-next-line @typescript-eslint/only-throw-error
+		if (!user.userId) throw new UploadThingError("Unauthorized");
+
+		return { userId: user.userId };
 		})
 		.onUploadComplete(async ({ metadata, file }) => {
-			console.log(metadata);
 			await db.insert(posts).values({
 				name: file.name,
 				url: file.url,
